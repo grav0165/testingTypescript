efbe9808e8c5e3cbb17dc772d4db2799bfdc3a65
Aubrey Graves <grav0165@gmail.com>
2023-11-12 12:43:09 -0600

    fixing typescript

--- sunday-src/server.ts
+++ sunday-src/server.ts
@@ -1,7 +1,7 @@
 import express, {Express, Request, Response, Application} from 'express';
 import dotenv from 'dotenv';
 
-dotenv.config();
+
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
