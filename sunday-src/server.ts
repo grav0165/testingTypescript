409f0d7576431db40d7715540c8f92c5e3c14016
Aubrey Graves <grav0165@gmail.com>
2023-11-12 12:19:32 -0600

    adding to server file for testing

---
+++ sunday-src/server.ts
@@ -0,0 +1,9 @@
+import express, {Express, Request, Response, Application} from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Application = express();
+const port = process.env.PORT || 8000;
+
+app.get('/', (req))
