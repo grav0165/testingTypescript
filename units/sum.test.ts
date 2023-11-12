3297b5106dd56913966e2b27a0726d047660a388
Aubrey Graves <grav0165@gmail.com>
2023-11-12 12:00:00 -0600

    adding describe block

--- units/sum.test.ts
+++ units/sum.test.ts
@@ -1,6 +1,8 @@
 import sum from '../sunday-testing/sunday.sum'
 
-test("Testing the application description", () => {
-    expect(sum(1,2)).toBe(3);
+describe("A bunch of different tests about basic function", () => {
+    test("Testing the application description", () => {
+        expect(sum(1, 2)).toBe(3);
+        expect(sum(3, 5)).toBeTruthy();
+    })
 })
-
