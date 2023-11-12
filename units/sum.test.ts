3c371eaa574de727c1230df48ad3a05a570c6d4c
Aubrey Graves <grav0165@gmail.com>
2023-11-12 12:08:12 -0600

    adding difference testing block

--- units/sum.test.ts
+++ units/sum.test.ts
@@ -1,8 +1,17 @@
 import sum from '../sunday-testing/sunday.sum'
+import difference from '../sunday-testing/sunday.difference'
 
 describe("A bunch of different tests about basic function", () => {
     test("Testing the application description", () => {
         expect(sum(1, 2)).toBe(3);
         expect(sum(3, 5)).toBeTruthy();
+    })
+    
+})
+
+
+describe("difference tests", () => {
+    test("Difference test functions as expected", () => {
+        expect(difference(4,2)).toBe(2);
     })
 })
