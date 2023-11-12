4a6259ce050468dbe453c48801418c01eb3e75de
Aubrey Graves <grav0165@gmail.com>
2023-11-12 12:27:29 -0600

    updated json for server typescript

--- sunday-src/server.ts
+++ sunday-src/server.ts
@@ -1,9 +1,16 @@
 import express, {Express, Request, Response, Application} from 'express';
-import dotenv from 'dotenv';
+import dotenv from '../dotenv
+';
 
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
-app.get('/', (req))
+app.get('/', (req: Request, res: Response) => {
+    res.send('Welcome to Express and Typescript server');
+});
+
+app.listen(port, () => {
+    console.log(`Server is Fire at http://localhost:${port}`);
+});
